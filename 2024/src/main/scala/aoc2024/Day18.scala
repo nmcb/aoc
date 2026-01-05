bdd210fb5f6a5466c9cb812baeabfe1b001d6148
nmcb <nmcb@xs4all.nl>
2026-01-05 13:17:06 +0100

    fixed test case

--- 2024/src/main/scala/aoc2024/Day18.scala
+++ 2024/src/main/scala/aoc2024/Day18.scala
@@ -10,20 +10,20 @@ object Day18 extends AoC:
   import Dijkstra.*
   import Pos.*
 
-  val bytes  = lines.map(_.split(',')).map(_.toPos).toVector
-  val memory = Grid.fill(71, 71, '.')
+  val bytes: Vector[Pos] = lines.map(_.split(',')).map(_.toPos)
+  val memory: Grid[Char] = Grid.fill(71, 71, '.')
 
   lazy val answer1: Int =
     val fallen = bytes.take(1024).foldLeft(memory)(_.updated(_, '#'))
     val graph  = Graph.fromGrid(fallen, '.')
-    val result = Dijkstra.run(graph, fallen.minPos)
+    val result = Dijkstra.run(fallen.minPos, graph)
     result.distanceTo(fallen.maxPos).get
 
   @tailrec
   def loop(todo: Vector[Pos], grid: Grid[Char]): Pos =
     val test     = grid.updated(todo.head, '.')
     val graph    = Graph.fromGrid(test, '.')
-    val distance = Dijkstra.run(graph, test.minPos).distanceTo(test.maxPos)
+    val distance = Dijkstra.run(test.minPos, graph).distanceTo(test.maxPos)
     if distance.isEmpty then loop(todo.tail, test) else todo.head
 
   lazy val answer2: Pos = loop(bytes.reverse, bytes.foldLeft(memory)(_.updated(_, '#')))
