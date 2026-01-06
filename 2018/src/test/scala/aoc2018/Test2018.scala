46e6a4ccea8326e73218f7daabf0a91284ce6cf8
nmcb <nmcb@xs4all.nl>
2026-01-06 15:03:07 +0100

    performance 2024 day 06; refactored test packages

--- 2018/src/test/scala/aoc2018/Test2018.scala
+++ 2018/src/test/scala/aoc2018/Test2018.scala
@@ -1,4 +1,5 @@
-import aoc2018.{Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, Day10, Day11, Day12, Day13, Day14, Day15, Day16, Day17, Day18, Day19, Day20, Day21, Day22, Day23, Day24, Day25}
+package aoc2018
+
 import org.scalatest.funsuite.AnyFunSuite
 
 class Test2018 extends AnyFunSuite:
