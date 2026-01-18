b4959dedfd51334703311015e277ec33fb97748d
nmcb <nmcb@xs4all.nl>
2026-01-18 23:25:32 +0100

    functional style chore

--- 2022/src/main/scala/aoc2022/Day06.scala
+++ 2022/src/main/scala/aoc2022/Day06.scala
@@ -1,30 +1,18 @@
 package aoc2022
 
 import nmcb.*
-
-import scala.annotation.tailrec
+import predef.*
 
 object Day06 extends AoC:
 
-  case class Move(size: Int, from: Int, to: Int)
-
-  def solve(s: String, d: Int): Int =
-    @tailrec
-    def loop(todo: String, last: String, count: Int = d): Int =
-      if todo.isEmpty then sys.error("not found")
-      else
-        val c = todo.head
-        val l = (last + c).drop(1)
-        if   count >= d && l.distinct.size == d then count + 1
-        else loop(todo.tail, l, count + 1)
-    loop(s.drop(d), s.take(d))
-
-  def solve1(s: String): Int =
-    solve(s, 4)
-
-  def solve2(s: String): Int =
-    solve(s, 14)
+  def solve(input: String, size: Int): Int =
+    input
+      .sliding(size)
+      .zipWithIndex
+      .find(_.element.distinct.length == size)
+      .get
+      .index + size
 
 
-  lazy val answer1: Int = solve1(input)
-  lazy val answer2: Int = solve2(input)
+  lazy val answer1: Int = solve(input, 4)
+  lazy val answer2: Int = solve(input, 14)
