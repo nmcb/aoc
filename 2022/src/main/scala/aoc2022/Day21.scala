ae57ce1d72428412a2e13ba76da0f4a49f236fb8
nmcb <nmcb@xs4all.nl>
2026-01-19 18:55:40 +0100

    naming

--- 2022/src/main/scala/aoc2022/Day21.scala
+++ 2022/src/main/scala/aoc2022/Day21.scala
@@ -7,15 +7,16 @@ import scala.io.*
 
 object Day21 extends AoC:
 
-  type Name = String
-  type Lazy = Long => Long
-  type Res  = Long | Lazy
+  type Name    = String
+  type Lazy    = Long => Long
+  type Result  = Long | Lazy
 
-  object Res:
-    extension (lhs: Res)
+  object Result:
+
+    extension (lhs: Result)
 
       @targetName("add")
-      def |+|(rhs: Res): Res =
+      def |+|(rhs: Result): Result =
         (lhs, rhs) match
           case (lo: Lazy, ro: Lazy) => lo.andThen(ro)
           case (ll: Long, ro: Lazy) => ro.compose(rl => rl - ll)
@@ -23,7 +24,7 @@ object Day21 extends AoC:
           case (ll: Long, rl: Long) => ll + rl
 
       @targetName("mul")
-      def |*|(rhs: Res): Res =
+      def |*|(rhs: Result): Result =
         (lhs, rhs) match
           case (lo: Lazy, ro: Lazy) => lo.andThen(ro)
           case (ll: Long, ro: Lazy) => ro.compose(rl => rl / ll)
@@ -31,7 +32,7 @@ object Day21 extends AoC:
           case (ll: Long, rl: Long) => ll * rl
 
       @targetName("sub")
-      def |-|(rhs: Res): Res =
+      def |-|(rhs: Result): Result =
         (lhs, rhs) match
           case (lo: Lazy, ro: Lazy) => lo.andThen(ro)
           case (ll: Long, ro: Lazy) => ro.compose(rl => ll - rl)
@@ -39,7 +40,7 @@ object Day21 extends AoC:
           case (ll: Long, rl: Long) => ll - rl
 
       @targetName("div")
-      def |/|(rhs: Res): Res =
+      def |/|(rhs: Result): Result =
         (lhs, rhs) match
           case (lo: Lazy, ro: Lazy) => ro.andThen(lo)
           case (ll: Long, ro: Lazy) => ro.compose(rl => ll / rl)
@@ -47,25 +48,30 @@ object Day21 extends AoC:
           case (ll: Long, rl: Long) => ll / rl
 
       @targetName("eq")
-      def |=|(rhs: Res): Res =
+      def |=|(rhs: Result): Result =
         (lhs, rhs) match
           case (ll: Long, ro: Lazy)             => ro(ll)
           case (lo: Lazy, rl: Long)             => lo(rl)
           case (ll: Long, rl: Long) if rl == ll => rl
           case _ => sys.error("boom!")
 
-  sealed abstract class Expr(val name: Name, line: String)
-    extends ((Name => Res) => Res):
-      override def toString: String = line + "\n"
+  type Defer      = Name  => Result
+  type Deferrable = Defer => Result
 
-  case class Val(override val name: Name, num: Long, line: String)
-    extends Expr(name, line):
-      def apply(defer: Name => Res): Res = num
+  sealed abstract class Expr(val name: Name, line: String) extends Deferrable:
 
-  case class Bin(override val name: Name, lhs: Name, rhs: Name, op: String, line: String)
-    extends Expr(name, line):
-      import Res.*
-      def apply(defer: Name => Res): Res =
+      override def toString: String =
+        s"$name=$name, line=$line\n"
+
+  case class Val(override val name: Name, result: Long, line: String) extends Expr(name, line):
+
+      def apply(defer: Defer): Result =
+        result
+
+  case class Bin(override val name: Name, lhs: Name, rhs: Name, op: String, line: String) extends Expr(name, line):
+      import Result.*
+
+      def apply(defer: Defer): Result =
         op match
           case "+" => defer(lhs) |+| defer(rhs)
           case "*" => defer(lhs) |*| defer(rhs)
@@ -74,13 +80,15 @@ object Day21 extends AoC:
           case "=" => defer(lhs) |=| defer(rhs)
 
   object Expr:
+
     def parseLine(line: String): Expr =
       line match
         case s"$n: $l $o $r" => Bin(n, l, r, o, s"$n: $l $o $r")
         case s"$n: $v"       => Val(n, v.toInt, s"$n: $v")
 
-  case class SAT(input: List[Expr]):
-    private val search: Name => Res =
+  case class SAT(input: Vector[Expr]):
+
+    private val search: Name => Result =
       name =>
         input
           .find(_.name == name)
@@ -92,17 +100,15 @@ object Day21 extends AoC:
         case l: Long => Some(l)
         case _: Lazy => None
 
-  val program: List[Expr] =
-    Source
-      .fromResource(s"$day.txt")
-      .getLines
-      .map(Expr.parseLine)
-      .toList
 
-  val patch: List[Expr] = program.flatMap:
-    case Bin("root", lhs, rhs, op, line) => Some(Bin("root", lhs, rhs, "=", line.replace(op.head, '=')))
-    case m if m.name == "humn"           => None
-    case m                               => Some(m)
+  val program: Vector[Expr] =
+    lines.map(Expr.parseLine)
+
+  val patch: Vector[Expr] =
+    program.flatMap:
+      case Bin("root", lhs, rhs, op, line) => Some(Bin("root", lhs, rhs, "=", line.replace(op.head, '=')))
+      case m if m.name == "humn"           => None
+      case m                               => Some(m)
 
 
   lazy val answer1: Long = SAT(program).solve.getOrElse(sys.error(s"unsolved ${program.foldLeft("\n")(_ + _)}"))
