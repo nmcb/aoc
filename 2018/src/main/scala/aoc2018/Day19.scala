19fedd9827e5db0d2b92d9eeac59b2c87023bf42
nmcb <nmcb@xs4all.nl>
2026-01-25 12:38:17 +0100

    cleanup 2022

--- 2018/src/main/scala/aoc2018/Day19.scala
+++ 2018/src/main/scala/aoc2018/Day19.scala
@@ -4,26 +4,8 @@ import nmcb.*
 import scala.annotation.tailrec
 
 object Day19 extends AoC:
-  
+
   import Day16.*
-
-  type Codes = (Int, Int, Int, Int)
-
-  extension (codes: Codes)
-    def opcode: Int = codes._1
-    def a: Int = codes._2
-    def b: Int = codes._3
-    def c: Int = codes._4
-
-  type Mem   = Map[Int,Int]
-
-  extension (mem: Mem)
-    private def valueOf(reg: Int): Int                      = mem.getOrElse(reg, 0)
-    def setRI(a: Int, b: Int, c: Int, f: Int => Int => Int) = mem.updated(c, f(mem.valueOf(a))(b))
-    def setIR(a: Int, b: Int, c: Int, f: Int => Int => Int) = mem.updated(c, f(a)(mem.valueOf(b)))
-    def setRR(a: Int, b: Int, c: Int, f: Int => Int => Int) = mem.updated(c, f(mem.valueOf(a))(mem.valueOf(b)))
-    def setRN(a: Int, c: Int)                               = mem.updated(c, mem.valueOf(a))
-    def setIN(a: Int, c: Int)                               = mem.updated(c, a)
 
   enum Inst:
     case ADDR(a: Int, b: Int, c: Int)
@@ -62,9 +44,6 @@ object Day19 extends AoC:
         case EQRI(a, b, c) => mem.setRI(a, b, c, (a: Int) => (b: Int) => if a == b then 1 else 0)
         case EQRR(a, b, c) => mem.setRR(a, b, c, (a: Int) => (b: Int) => if a == b then 1 else 0)
 
-  import Inst.*
-
-  var i = 0
   case class CPU(pragma: Int, program: Vector[Inst], pc: Int = 0, mem: Mem = Map.empty):
 
     def halted: Boolean = !program.indices.contains(pc)
@@ -82,30 +61,35 @@ object Day19 extends AoC:
       else
         execute(program(pc)).run
 
-  val cpu: CPU =
-    val pragma = lines(0) match
-      case s"#ip $r" => r.toInt
+  object CPU:
 
-    val program = lines.collect:
-      case s"$inst $a $b $c" => inst.toUpperCase match
-        case "ADDR" => ADDR(a.toInt, b.toInt, c.toInt)
-        case "ADDI" => ADDI(a.toInt, b.toInt, c.toInt)
-        case "MULR" => MULR(a.toInt, b.toInt, c.toInt)
-        case "MULI" => MULI(a.toInt, b.toInt, c.toInt)
-        case "BANR" => BANR(a.toInt, b.toInt, c.toInt)
-        case "BANI" => BANI(a.toInt, b.toInt, c.toInt)
-        case "BORR" => BORR(a.toInt, b.toInt, c.toInt)
-        case "BORI" => BORI(a.toInt, b.toInt, c.toInt)
-        case "SETR" => SETR(a.toInt,          c.toInt)
-        case "SETI" => SETI(a.toInt,          c.toInt)
-        case "GTIR" => GTIR(a.toInt, b.toInt, c.toInt)
-        case "GTRI" => GTRI(a.toInt, b.toInt, c.toInt)
-        case "GTRR" => GTRR(a.toInt, b.toInt, c.toInt)
-        case "EQIR" => EQIR(a.toInt, b.toInt, c.toInt)
-        case "EQRI" => EQRI(a.toInt, b.toInt, c.toInt)
-        case "EQRR" => EQRR(a.toInt, b.toInt, c.toInt)
+    import Inst.*
 
-    CPU(pragma, program)
+    def load(lines: Vector[String]): CPU =
+      val pragma = lines.collect:
+        case s"#ip $r" => r.toInt
+
+      val program = lines.collect:
+        case s"$inst $a $b $c" =>
+          inst.toUpperCase match
+            case "ADDR" => ADDR(a.toInt, b.toInt, c.toInt)
+            case "ADDI" => ADDI(a.toInt, b.toInt, c.toInt)
+            case "MULR" => MULR(a.toInt, b.toInt, c.toInt)
+            case "MULI" => MULI(a.toInt, b.toInt, c.toInt)
+            case "BANR" => BANR(a.toInt, b.toInt, c.toInt)
+            case "BANI" => BANI(a.toInt, b.toInt, c.toInt)
+            case "BORR" => BORR(a.toInt, b.toInt, c.toInt)
+            case "BORI" => BORI(a.toInt, b.toInt, c.toInt)
+            case "SETR" => SETR(a.toInt,          c.toInt)
+            case "SETI" => SETI(a.toInt,          c.toInt)
+            case "GTIR" => GTIR(a.toInt, b.toInt, c.toInt)
+            case "GTRI" => GTRI(a.toInt, b.toInt, c.toInt)
+            case "GTRR" => GTRR(a.toInt, b.toInt, c.toInt)
+            case "EQIR" => EQIR(a.toInt, b.toInt, c.toInt)
+            case "EQRI" => EQRI(a.toInt, b.toInt, c.toInt)
+            case "EQRR" => EQRR(a.toInt, b.toInt, c.toInt)
+
+      CPU(pragma.head, program)
 
   /**
    * @see Credits - https://www.reddit.com/r/adventofcode/comments/a7j9zc/comment/ec45g4d/
@@ -128,5 +112,5 @@ object Day19 extends AoC:
       if n % x == 0 then if n / x < x then total else go(x + 1, total + x + n / x) else go(x + 1, total)
     go()
 
-  override lazy val answer1: Int = cpu.run.mem.valueOf(0)
+  override lazy val answer1: Int = CPU.load(lines).run.mem.valueOf(0)
   override lazy val answer2: Int = sumOfFactorsOf(nPart2)
