5ea47f576e345d6141cd9c8584a0296bcd4ec9ca
nmcb <nmcb@xs4all.nl>
2026-02-06 13:52:24 +0100

    prefer extension methods

--- 2016/src/main/scala/aoc2016/Day06.scala
+++ 2016/src/main/scala/aoc2016/Day06.scala
@@ -4,37 +4,32 @@ import nmcb.*
 
 object Day06 extends AoC:
 
-  type CharCount = (Char,Int)
+  type CharCount = (Char, Int)
 
   extension (cc: CharCount)
     def char: Char = cc._1
     def count: Int = cc._2
 
-  case class Counter(counts: Map[Char,Int] = Map.empty):
+  type Counter = Map[Char, Int]
+
+  extension (counter: Counter)
 
     infix def add(char: Char): Counter =
-      Counter(
-        counts.updatedWith(char):
-          case Some(count) => Some(count + 1)
-          case None        => Some(1)
-      )
+      counter.updatedWith(char):
+        case Some(count) => Some(count + 1)
+        case None        => Some(1)
 
-    def max: Char =
-      counts.toVector.maxBy(_.count).char
+    def maxChar: Char =
+      counter.toVector.maxBy(_.count).char
 
-    def min: Char =
-      counts.toList.minBy(_.count).char
-
-  object Counter:
-
-    val empty: Counter =
-      Counter()
+    def minChar: Char =
+      counter.toList.minBy(_.count).char
 
   def counted(input: Vector[String]): Vector[Counter] =
     input
       .map(_.toVector)
       .transpose
-      .map(_.foldLeft(Counter.empty)(_ add _))
+      .map(_.foldLeft(Map.empty)(_ add _))
 
-  override lazy val answer1: String = counted(lines).map(_.max).mkString
-  override lazy val answer2: String = counted(lines).map(_.min).mkString
+  override lazy val answer1: String = counted(lines).map(_.maxChar).mkString
+  override lazy val answer2: String = counted(lines).map(_.minChar).mkString
