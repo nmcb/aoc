477a6d2d5be73463c83db365d83850dac328964c
nmcb <nmcb@xs4all.nl>
2026-02-22 11:19:23 +0100

    cleanup

--- 2023/src/main/scala/aoc2023/Day22.scala
+++ 2023/src/main/scala/aoc2023/Day22.scala
@@ -9,17 +9,16 @@ object Day22 extends AoC:
 
   case class Stack(stack: Vector[Box]):
 
-    val settled: Vector[Box] =
+    lazy val settled: Vector[Box] =
       stack
         .sortBy(_.min.z)
         .foldLeft(Vector.empty[Box]): (dropped, box) =>
-          val height =
-            Iterator
-              .from(1)
-              .findFirst: h =>
-                val dropping = box.drop(h)
-                dropping.min.z == 0 || dropped.exists(_ intersects dropping)
-          dropped :+ box.drop(height)
+          dropped :+ box.drop(
+            Iterator.from(1).findFirst: height =>
+              val dropping = box.drop(height)
+              dropping.min.z == 0 || dropped.exists(_ intersects dropping)
+            - 1
+          )
 
     val supportedBy: Map[Box, Set[Box]] =
       settled
@@ -62,27 +61,27 @@ object Day22 extends AoC:
             val add     = support.filter(b => (supportedBy(b) -- visited).isEmpty)
             loop(next ++ add, visited)
 
-        loop(Set(box))
+        loop(todo = Set(box))
 
       settled.map(disintegrate).sum
 
 
   val stack: Stack = Stack(lines.map(Box.fromString))
-  
+
   override lazy val answer1: Int = stack.disintegrable
   override lazy val answer2: Int = stack.disintegrated
 
   // Geometry
 
-  case class Pos3(x: Int, y: Int, z: Int):
-    def -(that: Pos3): Pos3         = Pos3(x - that.x, y - that.y, z - that.z)
-    def +(that: Pos3): Pos3         = Pos3(x + that.x, y + that.y, z + that.z)
-    infix def min(that: Pos3): Pos3 = Pos3(x min that.x, y min that.y, z min that.z)
-    infix def max(that: Pos3): Pos3 = Pos3(x max that.x, y max that.y, z max that.z)
-    def >=(that: Pos3): Boolean     = x >= that.x && y >= that.y && z >= that.z
-    def <=(that: Pos3): Boolean     = x <= that.x && y <= that.y && z <= that.z
+  case class Vec3(x: Int, y: Int, z: Int):
+    def -(that: Vec3): Vec3         = Vec3(x - that.x, y - that.y, z - that.z)
+    def +(that: Vec3): Vec3         = Vec3(x + that.x, y + that.y, z + that.z)
+    infix def min(that: Vec3): Vec3 = Vec3(x min that.x, y min that.y, z min that.z)
+    infix def max(that: Vec3): Vec3 = Vec3(x max that.x, y max that.y, z max that.z)
+    def >=(that: Vec3): Boolean     = x >= that.x && y >= that.y && z >= that.z
+    def <=(that: Vec3): Boolean     = x <= that.x && y <= that.y && z <= that.z
 
-  case class Box(min: Pos3, max: Pos3) derives CanEqual:
+  case class Box(min: Vec3, max: Vec3) derives CanEqual:
 
     def intersect(that: Box): Option[Box] =
       val maxMin = min max that.min
@@ -100,14 +99,14 @@ object Day22 extends AoC:
     def fromString(s: String): Box =
       s match
         case s"$x1,$y1,$z1~$x2,$y2,$z2" =>
-          val p1 = Pos3(x1.toInt, y1.toInt, z1.toInt)
-          val p2 = Pos3(x2.toInt, y2.toInt, z2.toInt)
+          val p1 = Vec3(x1.toInt, y1.toInt, z1.toInt)
+          val p2 = Vec3(x2.toInt, y2.toInt, z2.toInt)
           Box(p1, p2)
 
   extension (box: Box)
 
     def drop(height: Int): Box =
-      val offset = Pos3(0, 0, height)
+      val offset = Vec3(0, 0, height)
       Box(box.min - offset, box.max - offset)
 
     def drop1: Box =
