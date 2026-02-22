6bb93d281f2bd4a600de0bf7e3f36891aad492e7
nmcb <nmcb@xs4all.nl>
2026-02-22 10:15:52 +0100

    cleanup

--- 2023/src/main/scala/aoc2023/Day21.scala
+++ 2023/src/main/scala/aoc2023/Day21.scala
@@ -8,90 +8,89 @@ import scala.annotation.tailrec
 /** @see Credits for part 2 - https://github.com/merlinorg */
 object Day21 extends AoC:
 
-  case class Garden(grid: Vector[Vector[Char]]):
-    val sizeX: Int = grid.head.size
-    val sizeY: Int = grid.size
-
-    val positions: Set[Pos] =
-      val result =
-        for
-          y <- 0 until sizeY
-          x <- 0 until sizeX
-        yield
-          Pos.of(x, y)
-      result.toSet
-
-    lazy val startPos: Pos =
-      positions
-        .find(p => get(p) contains 'S')
-        .getOrElse(sys.error("no start position"))
-
-    def get(p: Pos): Option[Char] =
-      grid.lift(p.y).flatMap(_.lift(p.x))
+  extension (garden: Grid[Char])
 
     def rock(p: Pos): Boolean =
-      get(p).exists(_ == '#')
+      garden.peekOption(p).contains('#')
 
     def plot(p: Pos): Boolean =
-      get(p).exists(c => c == '.' || c == 'S')
+      garden.peekOption(p).exists(c => c == '.' || c == 'S')
 
-    /** part 1 - simple bfs */
+  /** part 1 - simple bfs */
 
-    def solve1(steps: Int): Int =
-      @tailrec
-      def loop(count: Int, found: Set[Pos] = Set(startPos)): Set[Pos] =
-        if count == 0 then found else loop(count - 1, found.flatMap(_.adjoint4.filter(plot)))
-      loop(steps).size
+  def solve1(garden: Grid[Char], steps: Long): Long =
+
+    @tailrec
+    def loop(count: Long, found: Set[Pos] = Set(garden.findOne('S'))): Set[Pos] =
+      if count == 0 then
+        found
+      else
+        loop(count - 1, found.flatMap(_.adjoint4.filter(garden.plot)))
+
+    loop(steps).size
 
 
-    /** part 2 - utilises the input grid being square and the solution scaling quadratically in grid size */
+  /** part 2 - utilises the input grid being square and the solution scaling quadratically in grid size */
 
-    assert(sizeX == sizeY)
-    val gridSize: Int = sizeY
+  extension (garden: Grid[Char])
+
+    def gridSize: Int =
+      assert(garden.sizeX == garden.sizeY)
+      garden.sizeY
 
     /** provides for an infinite garden using this grid as tiles */
     def infinite(p: Pos): Char =
-      val wrapX: Int = (p.x % sizeX + sizeX) % sizeX
-      val wrapY: Int = (p.y % sizeY + sizeY) % sizeY
-      grid(wrapY)(wrapX)
+      val wrapX: Int = (p.x % garden.sizeX + garden.sizeX) % garden.sizeX
+      val wrapY: Int = (p.y % garden.sizeY + garden.sizeY) % garden.sizeY
+      garden.peek(wrapX, wrapY)
 
-    final case class Grid(steps: Int = 0, plots: Set[Pos] = Set(startPos)):
-      def next: Grid =
-        copy(steps + 1,
+  case class Garden(grid: Grid[Char], steps: Int, plots: Set[Pos]):
+
+    def next: Garden =
+      copy(
+        steps = steps + 1,
+        plots =
           for
             plot <- plots
             step <- plot.adjoint4
-            if infinite(step) != '#'
-          yield step)
+            if grid.infinite(step) != '#'
+          yield
+            step
+      )
 
-    case class Collect(steps: Long, gridPlotsScan: Vector[Long] = Vector.empty):
-      val stepsToGrids: Long = steps / gridSize
-      val gridsToSteps: Long = steps % gridSize
-
-      infix def add(grid: Grid): Collect =
-        if grid.steps % gridSize == gridsToSteps then
-          copy(gridPlotsScan = gridPlotsScan :+ grid.plots.size)
-        else
-          this
-
-      /** given three distinct subsequent `y` points on a quadratic polynomial, solve `y` for given `x` */
-      def quadratic(y0: Long, y1: Long, y2: Long)(x: Long): Long =
-        y0 + (y1 - y0) * x + (x * (x - 1) / 2) * (y2 - 2 * y1 + y0)
-
-      def solution: Option[Long] =
-        gridPlotsScan match
-          case Vector(plots0, plots1, plots2) => Some(quadratic(plots0, plots1, plots2)(stepsToGrids))
-          case _                              => None
-
-    def solve2(steps: Int): Long =
-      Iterator
-        .iterate(Grid())(_.next)
-        .scanLeft(Collect(steps))(_ add _)
-        .flatMap(_.solution)
-        .next
-
-  val garden: Garden = Garden(lines.map(_.toVector))
+  object Garden:
+    def make(grid: Grid[Char]): Garden =
+      Garden(grid = grid, steps = 0, plots = Set(grid.findOne('S')))
 
 
-  override lazy val answer1: Int = garden.solve1(64)
-  override lazy val answer2: Long = garden.solve2(26501365)
+  case class Collect(grid: Grid[Char], steps: Long, gridPlotsScan: Vector[Long] = Vector.empty):
+    val stepsToGrids: Long = steps / grid.gridSize
+    val gridsToSteps: Long = steps % grid.gridSize
+
+    infix def add(garden: Garden): Collect =
+      if garden.steps % grid.gridSize == gridsToSteps then
+        copy(gridPlotsScan = gridPlotsScan :+ garden.plots.size)
+      else
+        this
+
+    /** given three distinct subsequent `y` points on a quadratic polynomial, solve `y` for given `x` */
+    def quadratic(y0: Long, y1: Long, y2: Long)(x: Long): Long =
+      y0 + (y1 - y0) * x + (x * (x - 1) / 2) * (y2 - 2 * y1 + y0)
+
+    def solution: Option[Long] =
+      gridPlotsScan match
+        case Vector(plots0, plots1, plots2) => Some(quadratic(plots0, plots1, plots2)(stepsToGrids))
+        case _                              => None
+
+  def solve2(grid: Grid[Char], steps: Int): Long =
+    Iterator
+      .iterate(Garden.make(grid))(_.next)
+      .scanLeft(Collect(grid, steps))(_ add _)
+      .flatMap(_.solution)
+      .next
+
+  val garden: Grid[Char] = Grid.fromLines(lines)
+
+
+  override lazy val answer1: Long = solve1(garden, 64)
+  override lazy val answer2: Long = solve2(garden, 26501365)
