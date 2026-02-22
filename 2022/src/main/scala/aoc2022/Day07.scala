edb5311b3ec1c8b48258bcc0c687ab9f410031b4
nmcb <nmcb@xs4all.nl>
2026-02-22 12:37:05 +0100

    cleanup

--- 2022/src/main/scala/aoc2022/Day07.scala
+++ 2022/src/main/scala/aoc2022/Day07.scala
@@ -4,20 +4,23 @@ import nmcb.*
 
 object Day07 extends AoC:
 
-  sealed trait Line
-  case object LsLine                             extends Line
-  case class  CdLine(name: String)               extends Line
-  case class  FileLine(name: String, size: Long) extends Line
-  case class  DirLine(name: String)              extends Line
+  enum Line:
+    case LsLine
+    case CdLine(name: String)
+    case FileLine(name: String, size: Long)
+    case DirLine(name: String)
 
-  def parseLine(l: String): Line =
-    l match
+  import Line.*
+
+  def fromString(line: String): Line =
+    line match
       case s"$$ cd $name"                      => CdLine(name)
       case s"$$ ls"                            => LsLine
       case s"$size $name" if size.head.isDigit => FileLine(name, size.toLong)
       case s"dir $name"                        => DirLine(name)
 
-  val stdout: List[Line] = lines.map(parseLine).toList
+  val stdout: Vector[Line] =
+    lines.map(fromString)
 
   type Path = String
 
@@ -27,38 +30,39 @@ object Day07 extends AoC:
     val slash = "/"
     val up    = ".."
 
-    def flatten(l: List[String]): String = l.mkString(slash)
+    def flatten(lines: Vector[String]): String =
+      lines.mkString(slash)
 
   case class File(name: Path, size: Long)
 
-  case class FileSystem(cur: List[String], fs: List[File] = List.empty, ds: List[Path]):
+  case class FileSystem(cur: Vector[String], fs: Vector[File] = Vector.empty, ds: Vector[Path]):
 
-    private val path2Size: Map[String,Long] =
+    private val path2Size: Map[String, Long] =
       ds.map(name => name -> sizeOf(name)).toMap
 
     def sizeOf(path: String): Long =
       fs.filter(_.name.startsWith(path)).map(_.size).sum
 
-    val sizes: List[Long] =
-      path2Size.view.values.toList
+    val sizes: Vector[Long] =
+      path2Size.valuesIterator.toVector
 
   object FileSystem:
-    val empty = FileSystem(List(Path.empty), List.empty, List(Path.root))
+    val empty: FileSystem =
+      FileSystem(Vector(Path.empty), Vector.empty, Vector(Path.root))
 
   lazy val fileSystem: FileSystem =
     import Path.*
     stdout
-      .foldLeft(FileSystem.empty)((sys, line) =>
+      .foldLeft(FileSystem.empty): (sys, line) =>
         line match
           case DirLine(name)        => sys.copy(ds = sys.ds :+ flatten(sys.cur :+ name))
           case FileLine(name, size) => sys.copy(fs = sys.fs :+ File(flatten(sys.cur :+ name), size))
           case CdLine(Path.up)      => sys.copy(cur = sys.cur.dropRight(1))
           case CdLine(name)         => sys.copy(cur = sys.cur :+ name)
           case _                    => sys
-      )
 
 
-  lazy val free: Long = 70000000L - fileSystem.sizeOf("/")
+  lazy val free: Long  = 70000000L - fileSystem.sizeOf("/")
   lazy val clean: Long = 30000000L - free
 
   override lazy val answer1: Long = fileSystem.sizes.filter(_ <= 100000L).sum
