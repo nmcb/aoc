b251a17092542ade030745451a7fa3aac58971d3
nmcb <nmcb@xs4all.nl>
2026-02-22 12:17:26 +0100

    cleanup

--- 2022/src/main/scala/aoc2022/Day04.scala
+++ 2022/src/main/scala/aoc2022/Day04.scala
@@ -4,16 +4,15 @@ import nmcb.*
 
 object Day04 extends AoC:
 
-  val sections: Vector[(Range,Range)] =
-    lines.map:
-      case s"$l1-$r1,$l2-$r2" => (l1.toInt to r1.toInt, l2.toInt to r2.toInt)
+  val sections: Vector[(Range, Range)] = lines.collect:
+    case s"$l1-$r1,$l2-$r2" => (l1.toInt to r1.toInt, l2.toInt to r2.toInt)
 
-  def contained1(r1: Range, r2: Range): Boolean =
-    r1.forall(i => r2.contains(i)) | r2.forall(i => r1.contains(i))
+  def contains1(r1: Range, r2: Range): Boolean =
+    r1.forall(r2.contains) || r2.forall(r1.contains)
 
-  def contained2(r1: Range, r2: Range): Boolean =
-    r1.exists(i => r2.contains(i))
+  def contains2(r1: Range, r2: Range): Boolean =
+    r1.exists(r2.contains)
 
 
-  override lazy val answer1: Int = sections.count(contained1)
-  override lazy val answer2: Int = sections.count(contained2)
+  override lazy val answer1: Int = sections.count(contains1)
+  override lazy val answer2: Int = sections.count(contains2)
