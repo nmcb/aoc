9fbdf1bb36920e06c7283cff90119e72860c3af9
nmcb <nmcb@xs4all.nl>
2026-02-27 00:21:20 +0100

    fix test

--- 2015/src/main/scala/aoc2015/Day18.scala
+++ 2015/src/main/scala/aoc2015/Day18.scala
@@ -1,7 +1,6 @@
 package aoc2015
 
 import nmcb.*
-import nmcb.pos.{*, given}
 
 import scala.annotation.tailrec
 
@@ -38,7 +37,7 @@ object Day18 extends AoC:
       Vector.empty[Row]
 
   /** grid encapsulates the animation of a light configuration */
-  case class Grid(conf: Conf, overlay: Set[Pos] = Set.empty):
+  case class Grid(conf: Conf, overlay: Set[(Int,Int)] = Set.empty):
     val sizeX: Int = conf.map(_.size).max
     val sizeY: Int = conf.size
     val minX: Int  = 0
@@ -46,13 +45,13 @@ object Day18 extends AoC:
     val maxX: Int  = sizeX - 1
     val maxY: Int  = sizeY - 1
 
-    def withOverlay(on: Pos*): Grid =
+    def withOverlay(on: (Int,Int)*): Grid =
       copy(overlay = Set.from(on))
 
-    def fold[A](zero: A)(inc: A => A)(f: (Int, Int, A) => A): A =
+    def fold[A](zero: A)(inc: A => A)(f: (Int,Int,A) => A): A =
       (minY to maxY).foldLeft(zero): (z, y) =>
         (minX to maxX).foldLeft(inc(z)): (z, x) =>
-          f(x, y, z)
+          f(x,y,z)
 
     def count: Int =
       fold(0)(identity): (x, y, n) =>
@@ -62,16 +61,15 @@ object Day18 extends AoC:
       if overlay(y, x) then on else conf(y)(x)
 
     def neighbours(x: Int, y: Int): Vector[Light] =
-      Pos.offset8
+      Vector((-1, -1), (0, -1), (1, -1), (-1, 0),(1, 0), (-1, 1), (0, 1), (1, 1))
         .map((dx, dy) => (x + dx, y + dy))
         .filter((px, py) => px >= minX && px <= maxX && py >= minY && py <= maxY)
         .map(light)
-        .toVector
 
     def next: Grid =
       val step: Conf =
-        fold(Conf.empty)(_ :+ Row.empty)((x, y, conf) =>
-          conf.init :+ (conf.last :+ light(x, y).next(neighbours(x, y))))
+        fold(Conf.empty)(_ :+ Row.empty)((x,y,conf) =>
+          conf.init :+ (conf.last :+ light(x,y).next(neighbours(x,y))))
       copy(conf = step)
 
     @tailrec
