ad951d356180f7028d012a67d06d4787604227a0
nmcb <nmcb@xs4all.nl>
2026-03-01 09:06:02 +0100

    cleanup

--- 2017/src/main/scala/aoc2017/Day05.scala
+++ 2017/src/main/scala/aoc2017/Day05.scala
@@ -1,21 +1,19 @@
 package aoc2017
 
 import nmcb.*
+
 import scala.annotation.tailrec
 
 object Day05 extends AoC:
 
-  val jumps: Vector[Int] =
-    lines.map(_.trim.toInt)
+  val jumps: Vector[Int] = lines.map(_.trim.toInt)
 
   case class CPU(mem: Vector[Int], update: Int => Int, pc: Int = 0, steps: Int = 0):
     @tailrec
     final def run: CPU =
       mem.lift(pc) match
-        case None =>
-          this
-        case Some(offset) =>
-          CPU(mem.updated(pc, update(offset)), update, pc + offset, steps + 1).run
+        case None         => this
+        case Some(offset) => CPU(mem.updated(pc, update(offset)), update, pc + offset, steps + 1).run
 
   override lazy val answer1: Int = CPU(jumps, offset => offset + 1).run.steps
   override lazy val answer2: Int = CPU(jumps, offset => if offset >= 3 then offset - 1 else offset + 1).run.steps
