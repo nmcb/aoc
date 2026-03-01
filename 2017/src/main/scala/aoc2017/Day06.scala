a844ef715d875deb062b08f423856dde0240bd46
nmcb <nmcb@xs4all.nl>
2026-03-01 09:09:20 +0100

    cleanup

--- 2017/src/main/scala/aoc2017/Day06.scala
+++ 2017/src/main/scala/aoc2017/Day06.scala
@@ -5,19 +5,16 @@ import scala.annotation.tailrec
 
 object Day06 extends AoC:
 
-  val banks: Vector[Int] =
-    input
-      .split("\\s")
-      .map(_.trim.toInt)
-      .toVector
+  val banks: Vector[Int] = input.split("\\s").map(_.trim.toInt).toVector
 
   case class Area(banks: Vector[Int]) derives CanEqual:
 
-    extension (index: Int) private def next: Int =
-      if index + 1 >= banks.size then 0 else index + 1
+    extension (index: Int)
+      def next: Int =
+        if index + 1 >= banks.size then 0 else index + 1
 
     @tailrec
-    private def distribute(amount: Int, index: Int, accumulator: Vector[Int]): Vector[Int] =
+    final def distribute(amount: Int, index: Int, accumulator: Vector[Int]): Vector[Int] =
       if amount <= 0 then
         accumulator
       else
@@ -41,10 +38,14 @@ object Day06 extends AoC:
           loop(next, seen :+ current)
       loop(this)
 
-  override lazy val answer1: Int =
+  def solve1(banks: Vector[Int]): Int =
     Area(banks).redistribute.size - 1
 
-  override lazy val answer2: Int =
+  def solve2(banks: Vector[Int]): Int =
     val seen = Area(banks).redistribute
     val last = seen.last
     seen.dropWhile(_ != last).size - 1
+
+  
+  override lazy val answer1: Int = solve1(banks)
+  override lazy val answer2: Int = solve2(banks)
