ee67d5f0a44aea3884d485794c5a14861fc681c9
nmcb <nmcb@xs4all.nl>
2026-03-02 15:41:25 +0100

    cleanup

--- 2019/src/main/scala/aoc2019/Day06.scala
+++ 2019/src/main/scala/aoc2019/Day06.scala
@@ -1,6 +1,7 @@
 package aoc2019
 
 import nmcb.*
+import nmcb.predef.*
 
 import scala.annotation.tailrec
 
@@ -10,31 +11,27 @@ object Day06 extends AoC:
 
   case class Planet(name: Name, center: Name)
 
-  val planets: Vector[Planet] = lines.collect:
-    case s"$center)$name" => Planet(name, center)
+  extension (planets: Vector[Planet])
 
-  def find(p: Planet => Boolean): Planet =
-    planets.find(p).get
+    def orbits: Map[Name, Vector[Name]] =
+      @tailrec
+      def loop(p: Planet, acc: Vector[Name] = Vector.empty): Vector[Name] =
+        if (p.center == "COM")
+          p.name +: acc
+        else
+          val next = planets.find(_.name == p.center).get
+          loop(next, next.name +: acc)
+      planets.map(p => p.name -> loop(p)).toMap
 
-  def orbits(p: Planet): Vector[Name] =
-    @tailrec
-    def loop(p: Planet, acc: Vector[Name] = Vector.empty): Vector[Name] =
-      if (p.center == "COM")
-        p.name +: acc
-      else
-        val next = find(_.name == p.center)
-        loop(next, next.name +: acc)
-    loop(p)
-
-  def pathToCom(n: Name): Vector[Name] =
-    @tailrec
-    def loop(n: Name, acc: Vector[Name] = Vector.empty): Vector[Name] =
-      val c = find(_.name == n).center
-      if (c == "COM")
-        acc
-      else
-        loop(c, c +: acc)
-    loop(n)
+    def pathToCom(n: Name): Vector[Name] =
+      @tailrec
+      def loop(n: Name, acc: Vector[Name] = Vector.empty): Vector[Name] =
+        val c = planets.find(_.name == n).get.center
+        if (c == "COM")
+          acc
+        else
+          loop(c, c +: acc)
+      loop(n)
 
   @tailrec
   def path(l: Vector[Name], r: Vector[Name]): Vector[Name] =
@@ -42,5 +39,9 @@ object Day06 extends AoC:
       case (l1 +: l2 +: _ , r1 +: r2 +: _) if l1 == r1 && l2 == r2 => path(l.tail, r.tail)
       case (l1 +: _       , r1 +: _      ) if l1 == r1             => l.reverse ++ r.tail
 
-  override lazy val answer1: Int = planets.map(orbits).map(_.length).sum
-  override lazy val answer2: Int = path(pathToCom("YOU"), pathToCom("SAN")).length
+  val planets: Vector[Planet] = lines.collect:
+    case s"$center)$name" => Planet(name, center)
+
+
+  override lazy val answer1: Int = planets.orbits.map(_.right.length).sum
+  override lazy val answer2: Int = path(planets.pathToCom("YOU"), planets.pathToCom("SAN")).length
