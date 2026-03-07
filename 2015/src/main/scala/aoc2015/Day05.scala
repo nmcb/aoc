4bd2180d906b0aa1899524465e81acf29172c3e7
nmcb <nmcb@xs4all.nl>
2026-03-07 18:03:33 +0100

    cleanup

--- 2015/src/main/scala/aoc2015/Day05.scala
+++ 2015/src/main/scala/aoc2015/Day05.scala
@@ -10,8 +10,16 @@ object Day05 extends AoC:
   val pair: UnanchoredRegex    = "(.)\\1".r.unanchored
   val naughty: UnanchoredRegex = "ab|cd|pq|xy".r.unanchored
 
-  val twoPair: UnanchoredRegex = "(..).*\\1".r.unanchored
-  val triple: UnanchoredRegex  = "(.).\\1".r.unanchored
+  val twoPair: UnanchoredRegex         = "(..).*\\1".r.unanchored
+  val enclosedTriple: UnanchoredRegex  = "(.).\\1".r.unanchored
 
-  override lazy val answer1: Int = lines.count(line => vowels.matches(line) && pair.matches(line) && !naughty.matches(line))
-  override lazy val answer2: Int = lines.count(line => twoPair.matches(line) && triple.matches(line))
+  def solve1(lines: Vector[String]): Int =
+    lines.count: line =>
+      vowels.matches(line) && pair.matches(line) && !naughty.matches(line)
+
+  def solve2(lines: Vector[String]): Int =
+    lines.count: line =>
+      twoPair.matches(line) && enclosedTriple.matches(line)
+
+  override lazy val answer1: Int = solve1(lines)
+  override lazy val answer2: Int = solve2(lines)
