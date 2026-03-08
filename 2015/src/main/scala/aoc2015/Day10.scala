d253468cb1b5e261d61eecb0241510c1e6c9af49
nmcb <nmcb@xs4all.nl>
2026-03-08 11:45:11 +0100

    cleanup

--- 2015/src/main/scala/aoc2015/Day10.scala
+++ 2015/src/main/scala/aoc2015/Day10.scala
@@ -5,14 +5,14 @@ import nmcb.predef.*
 
 object Day10 extends AoC:
 
-  val puzzle: String = "1113222113"
+  override lazy val input: String = "1113222113"
 
-  def lookAndSay(s: String): String =
+  def lookAndSay(number: String): String =
     val result = new StringBuilder
     var index  = 0
-    while index < s.length do
-      val char   = s(index)
-      val length = 1 + s.segmentLength(_ == char, index + 1)
+    while index < number.length do
+      val char   = number(index)
+      val length = 1 + number.segmentLength(_ == char, index + 1)
       result.append(length).append(char)
       index += length
     result.toString
@@ -20,5 +20,5 @@ object Day10 extends AoC:
   def solve(s: String, times: Int): Int =
     Iterator.iterate(s)(lookAndSay).nth(times).length
 
-  override lazy val answer1: Int = solve(puzzle, 40)
-  override lazy val answer2: Int = solve(puzzle, 50)
+  override lazy val answer1: Int = solve(input, 40)
+  override lazy val answer2: Int = solve(input, 50)
