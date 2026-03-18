70d4c4f03dc537449fbfc993b4b282746aa3be80
nmcb <nmcb@xs4all.nl>
2026-03-18 20:29:11 +0100

    test pr autorisatie

--- 2018/src/main/scala/aoc2018/Day01.scala
+++ 2018/src/main/scala/aoc2018/Day01.scala
@@ -5,9 +5,6 @@ import scala.annotation.tailrec
 
 object Day01 extends AoC:
 
-  val frequencies: Vector[Int] = lines.map(_.toInt)
-
-
   def firstFrequencyFoundTwice(frequencies: Vector[Int]): Int =
     @tailrec
     def loop(fs: Vector[Int], seen: Set[Int] = Set(0), result: Int = 0): Int =
@@ -16,6 +13,9 @@ object Day01 extends AoC:
         case freq +: _ if seen.contains(freq + result) => freq + result
         case freq +: rest                              => loop(rest, seen + (freq + result), freq + result)
     loop(frequencies)
+
+
+  val frequencies: Vector[Int] = lines.map(_.toInt)
 
   override lazy val answer1: Int = frequencies.sum
   override lazy val answer2: Int = firstFrequencyFoundTwice(frequencies)
