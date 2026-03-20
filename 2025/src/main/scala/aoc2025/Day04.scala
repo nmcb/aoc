2044f3a980e426c6c894ec30eddd5e6e216b55b8
nmc.borst <nmcb@xs4all.nl>
2026-03-20 21:00:22 +0100

    fix test (#166)
    * fix test
    
    * cleanup

--- 2025/src/main/scala/aoc2025/Day04.scala
+++ 2025/src/main/scala/aoc2025/Day04.scala
@@ -7,29 +7,16 @@ import scala.annotation.tailrec
 
 object Day04 extends AoC:
 
-  val rolls: Set[Pos] =
-    val ps = for y <- lines.indices ; x <- lines.head.indices yield (x = x, y = y)
-    ps.filter(p => lines(p.y)(p.x) == '@').toSet
-
-  val minPos: Pos =
-    rolls.min
-
-  val maxPos: Pos =
-    rolls.max
-
-  def neighbours(p: Pos, minPos: Pos, maxPos: Pos): Set[Pos] =
-    Pos.offset8.map(_ + p).filter(p => p >= minPos && p <= maxPos)
-
-  def neighbouring(rolls: Set[Pos], p: Pos, minPos: Pos, maxPos: Pos): Set[Pos] =
-    neighbours(p, minPos, maxPos).intersect(rolls)
-
-  def accessible(rolls: Set[Pos], minPos: Pos, maxPos: Pos): Set[Pos] =
-    rolls.filter(p => neighbouring(rolls, p, minPos, maxPos).size < 4)
+  def accessible(rolls: Set[Pos]): Set[Pos] =
+    rolls.filter(roll => roll.adjoint8.intersect(rolls).size < 4)
 
   @tailrec
-  def clearAll(rolls: Set[Pos], minPos: Pos, maxPos: Pos): Set[Pos] =
-    val remove = accessible(rolls, minPos, maxPos)
-    if remove.isEmpty then rolls else clearAll(rolls -- remove, minPos, maxPos)
+  def clearAll(rolls: Set[Pos]): Set[Pos] =
+    val remove = accessible(rolls)
+    if remove.isEmpty then rolls else clearAll(rolls -- remove)
 
-  override lazy val answer1: Int = accessible(rolls, minPos, maxPos).size
-  override lazy val answer2: Int = (accessible(rolls, minPos, maxPos) -- clearAll(rolls, minPos, maxPos)).size
+
+  val rolls: Set[Pos] = Grid.fromLines(lines).filter(_.element == '@').map(_.pos)
+
+  override lazy val answer1: Int = accessible(rolls).size
+  override lazy val answer2: Int = (rolls -- clearAll(rolls)).size
--- 2025/src/main/scala/aoc2025/Day12.scala
+++ 2025/src/main/scala/aoc2025/Day12.scala
@@ -5,13 +5,10 @@ import nmcb.*
 object Day12 extends AoC:
 
   case class Region(width: Int, height: Int, amounts: Vector[Int]):
-
     val volume: Int = width * height
 
   case class Present(pattern: Vector[Vector[Char]]):
-
-    val volume: Int =
-      pattern.map(_.count(_ == '#')).sum
+    val volume: Int = pattern.map(_.count(_ == '#')).sum
 
   extension (presents: Vector[Present])
 
@@ -25,16 +22,14 @@ object Day12 extends AoC:
     regions.count(region => presents.volume(region.amounts) <= region.volume)
 
   lazy val regions: Vector[Region] = chunks.last.collect:
-    case s"${w}x${h}: $nums" =>
+    case s"${w}x${h}: $numbers" =>
       Region(
-        width = w.toInt,
-        height = h.toInt,
-        amounts = nums.split(" ").map(_.toInt).toVector,
+        width   = w.toInt,
+        height  = h.toInt,
+        amounts = numbers.split(" ").map(_.toInt).toVector,
       )
 
-  lazy val presents: Vector[Present] = chunks.init
-    .map: lines =>
+  lazy val presents: Vector[Present] = chunks.init.collect: lines =>
       Present(lines.tail.map(_.toVector))
-
 
   override lazy val answer1: Int    = solve1(regions, presents)
