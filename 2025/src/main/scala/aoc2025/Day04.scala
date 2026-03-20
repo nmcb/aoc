e42c315e9b3678871a2224971d46587e6fa0fe74
nmcb <nmcb@xs4all.nl>
2026-03-20 20:54:47 +0100

    cleanup

--- 2025/src/main/scala/aoc2025/Day04.scala
+++ 2025/src/main/scala/aoc2025/Day04.scala
@@ -7,26 +7,16 @@ import scala.annotation.tailrec
 
 object Day04 extends AoC:
 
-  val positions: Seq[Pos] =
-    for
-      y <- lines.indices
-      x <- lines.head.indices
-    yield
-      (x = x, y = y)
-
-  def accessible(rolls: Set[Pos], minPos: Pos, maxPos: Pos): Set[Pos] =
-    def neighbouring(p: Pos): Set[Pos] = p.adjoint8.withinBounds(minPos, maxPos).intersect(rolls)
-    rolls.filter(p => neighbouring(p).size < 4)
+  def accessible(rolls: Set[Pos]): Set[Pos] =
+    rolls.filter(roll => roll.adjoint8.intersect(rolls).size < 4)
 
   @tailrec
-  def clearAll(rolls: Set[Pos], minPos: Pos, maxPos: Pos): Set[Pos] =
-    val remove = accessible(rolls, minPos, maxPos)
-    if remove.isEmpty then rolls else clearAll(rolls -- remove, minPos, maxPos)
+  def clearAll(rolls: Set[Pos]): Set[Pos] =
+    val remove = accessible(rolls)
+    if remove.isEmpty then rolls else clearAll(rolls -- remove)
 
 
-  val minPos: Pos = positions.min
-  val maxPos: Pos = positions.max
-  val rolls: Set[Pos] = positions.filter(p => lines(p.y)(p.x) == '@').toSet
+  val rolls: Set[Pos] = Grid.fromLines(lines).filter(_.element == '@').map(_.pos)
 
-  override lazy val answer1: Int = accessible(rolls, minPos, maxPos).size
-  override lazy val answer2: Int = (rolls -- clearAll(rolls, minPos, maxPos)).size
+  override lazy val answer1: Int = accessible(rolls).size
+  override lazy val answer2: Int = (rolls -- clearAll(rolls)).size
