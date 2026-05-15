696c8c1f9fd69a156b4f75b8145b8b311706a3a7
nmc.borst <nmcb@xs4all.nl>
2026-05-15 16:24:17 +0000

    cleanup (#194)
    * use grid
    
    * reuse grid
    
    * cleanup

--- 2021/src/main/scala/aoc2021/Day20.scala
+++ 2021/src/main/scala/aoc2021/Day20.scala
@@ -5,7 +5,7 @@ import nmcb.pos.*
 
 object Day20 extends AoC:
 
-  case class Image(pixels: Grid[Boolean], canvas: Boolean)
+  case class Image(grid: Grid[Boolean], canvas: Boolean)
 
   val offsets: Seq[Pos] =
     for
@@ -19,17 +19,16 @@ object Day20 extends AoC:
       (acc << 1) | (if bit then 1 else 0)
 
   def enhance(algorithm: Vector[Boolean], image: Image): Image =
-    val Image(pixels, canvas) = image
-    val frame    = Vector.fill(pixels.sizeX + 2)(canvas)
-    val framed   = Grid.fromMatrix(frame +: pixels.matrix.map(canvas +: _ :+ canvas) :+ frame)
+    val frame    = Vector.fill(image.grid.sizeX + 2)(image.canvas)
+    val framed   = Grid.fromMatrix(frame +: image.grid.matrix.map(image.canvas +: _ :+ image.canvas) :+ frame)
     val enhanced = framed.mapElement: (pos, cell) =>
       val area = offsets.map(_ + pos)
-      val bin  = area.map(cell => if framed.within(cell) then framed.peek(cell) else canvas)
+      val bin  = area.map(cell => if framed.within(cell) then framed.peek(cell) else image.canvas)
       algorithm(int(bin.toVector))
 
     val Int9x0 = 0
     val Int9x1 = 511
-    val background = algorithm(if canvas then Int9x1 else Int9x0)
+    val background = algorithm(if image.canvas then Int9x1 else Int9x0)
     Image(enhanced, background)
 
   def parseImage(s: String): Image =
@@ -41,8 +40,8 @@ object Day20 extends AoC:
     val image = parseImage(imageStr)
     (algorithm, image)
 
-  val (algorithm: Vector[Boolean], image0: Image) = parse(input)
+  val (algorithm: Vector[Boolean], image: Image) = parse(input)
 
 
-  override lazy val answer1: Int = enhance(algorithm, image0).pixels.count(_ == true)
-  override lazy val answer2: Int = (1 to 50).foldLeft(image0)((img, _) => enhance(algorithm, img)).pixels.count(_ == true)
+  override lazy val answer1: Int = enhance(algorithm, image).grid.count(_ == true)
+  override lazy val answer2: Int = (1 to 50).foldLeft(image)((img, _) => enhance(algorithm, img)).grid.count(_ == true)
