f91003a2c080450ddfebacdc2c37b32158ef8ec4
nmcb <nmcb@xs4all.nl>
2026-05-16 10:10:34 +0200

    cleanup

--- 2021/src/main/scala/aoc2021/Day20.scala
+++ 2021/src/main/scala/aoc2021/Day20.scala
@@ -1,47 +1,49 @@
 package aoc2021
 
 import nmcb.*
+import nmcb.predef.*
 import nmcb.pos.*
 
 object Day20 extends AoC:
 
-  case class Image(grid: Grid[Boolean], canvas: Boolean)
+  case class Image(algorithm: Vector[Boolean], grid: Grid[Boolean], canvas: Boolean):
 
-  val offsets: Seq[Pos] =
-    for
-      dy <- -1 to 1
-      dx <- -1 to 1
-    yield
-      (dx, dy)
+    import Image.*
 
-  def int(binary: Vector[Boolean]): Int =
-    binary.foldLeft(0): (acc, bit) =>
-      (acc << 1) | (if bit then 1 else 0)
+    extension (binary: Seq[Boolean])
+      def toInt: Int =
+        binary.foldLeft(0): (acc, bit) =>
+          (acc << 1) | (if bit then 1 else 0)
 
-  def enhance(algorithm: Vector[Boolean], image: Image): Image =
-    val frame    = Vector.fill(image.grid.sizeX + 2)(image.canvas)
-    val framed   = Grid.fromMatrix(frame +: image.grid.matrix.map(image.canvas +: _ :+ image.canvas) :+ frame)
-    val enhanced = framed.mapElement: (pos, cell) =>
-      val area = offsets.map(_ + pos)
-      val bin  = area.map(cell => if framed.within(cell) then framed.peek(cell) else image.canvas)
-      algorithm(int(bin.toVector))
+    def enhance: Image =
+      val frame  = Vector.fill(grid.sizeX + 2)(canvas)
+      val framed = Grid.fromMatrix(frame +: grid.matrix.map(canvas +: _ :+ canvas) :+ frame)
+      val enhanced = framed.mapElement: (pos, _) =>
+        val area   = offsets.map(_ + pos)
+        val binary = area.map(cell => if framed.within(cell) then framed.peek(cell) else canvas)
+        algorithm(binary.toInt)
 
-    val Int9x0 = 0
-    val Int9x1 = 511
-    val background = algorithm(if image.canvas then Int9x1 else Int9x0)
-    Image(enhanced, background)
+      val Int9x0 = 0
+      val Int9x1 = 511
+      val background = algorithm(if canvas then Int9x1 else Int9x0)
+      copy(grid = enhanced, canvas = background)
 
-  def parseImage(s: String): Image =
-    Image(Grid.fromLines(s.linesIterator).map(_ == '#'), false)
+  object Image:
 
-  def parse(input: String): (Vector[Boolean], Image) =
-    val Vector(algorithmStr, imageStr) = input.split("\n\n").toVector
-    val algorithm = algorithmStr.map(_ == '#').toVector
-    val image = parseImage(imageStr)
-    (algorithm, image)
+    def fromString(s: String): Image =
+      val Array(as, is) = s.split("\n\n")
+      val algorithm = as.map(_ == '#').toVector
+      val grid      = Grid.fromLines(is.linesIterator).map(_ == '#')
+      Image(algorithm, grid, false)
 
-  val (algorithm: Vector[Boolean], image: Image) = parse(input)
+    val offsets: Seq[Pos] =
+      for
+        dy <- -1 to 1
+        dx <- -1 to 1
+      yield
+        (dx, dy)
 
+  val image: Image = Image.fromString(input)
 
-  override lazy val answer1: Int = enhance(algorithm, image).grid.count(_ == true)
-  override lazy val answer2: Int = (1 to 50).foldLeft(image)((img, _) => enhance(algorithm, img)).grid.count(_ == true)
+  override lazy val answer1: Int = image.enhance.grid.count(_ == true)
+  override lazy val answer2: Int = Iterator.iterate(image)(_.enhance).nth(50).grid.count(_ == true)
