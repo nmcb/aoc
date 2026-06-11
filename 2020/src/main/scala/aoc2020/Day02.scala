819f9da1c6154a9d75cea9a1c49bd1fdab8e8c50
nmc.borst <nmcb@xs4all.nl>
2026-06-11 12:24:50 +0200

    add documenting comments (#211)

--- 2020/src/main/scala/aoc2020/Day02.scala
+++ 2020/src/main/scala/aoc2020/Day02.scala
@@ -10,17 +10,19 @@ object Day02 extends AoC:
 
   val Line: Regex = "(\\d+)-(\\d+)\\s(.):\\s(.+)".r
 
-  def answer(predicate: Predicate): Int = lines.count:
+  def solve(predicate: Predicate): Int = lines.count:
     case s"$x-$y $char: $password" => predicate(x.toInt)(y.toInt)(char.head)(password)
 
+  // check that given min and max number of given char are in given passwd
   def check1: Predicate = min => max => char => passwd =>
     val count = passwd.count(_ == char)
     count >= min && count <= max
-    
+
+  // check that exactly one of given (one-based) indices in given passwd contains given char
   def check2: Predicate = x => y => char => passwd =>
     val chars = passwd.zipWithIndex.map(_.swap).toMap
     chars.get(x - 1).contains(char) ^ chars.get(y - 1).contains(char)
 
 
-  override lazy val answer1: Int = answer(check1)
-  override lazy val answer2: Int = answer(check2)
+  override lazy val answer1: Int = solve(check1)
+  override lazy val answer2: Int = solve(check2)
