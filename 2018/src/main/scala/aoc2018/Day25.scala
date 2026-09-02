6626d403c414d978e4023723e7a49706862f2e36
nmcb <nmcb@xs4all.nl>
2026-09-02 04:26:34 +0200

    reorder parameters lexicographically

--- 2018/src/main/scala/aoc2018/Day25.scala
+++ 2018/src/main/scala/aoc2018/Day25.scala
@@ -4,18 +4,18 @@ import nmcb.*
 
 object Day25 extends AoC:
 
-  case class Dim4(x: Int, y: Int, z: Int, w: Int):
+  case class Dim4(w: Int, x: Int, y: Int, z: Int):
     def manhattan(that: Dim4): Int =
-      (x - that.x).abs + (y - that.y).abs + (z - that.z).abs + (w - that.w).abs
+      (w - that.w).abs + (x - that.x).abs + (y - that.y).abs + (z - that.z).abs
 
   def solve(positions: Vector[Dim4]): Int =
     positions
       .foldLeft(Set.empty[Set[Dim4]]): (constellations, pos) =>
-          val (near,far) = constellations.partition(_.exists(_.manhattan(pos) <= 3))
+          val (near, far) = constellations.partition(_.exists(_.manhattan(pos) <= 3))
           far + (near.flatten + pos)
       .size
 
   val positions: Vector[Dim4] = lines.map:
-    case s"$x,$y,$z,$w" => Dim4(x.toInt, y.toInt, z.toInt, w.toInt)
+    case s"$w,$x,$y,$z" => Dim4(w.toInt, x.toInt, y.toInt, z.toInt)
 
   override lazy val answer1: Int    = solve(positions)
