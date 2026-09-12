8e11dfdb540611d62b70be4767d1f07cb45f7b30
nmcb <nmcb@xs4all.nl>
2026-09-12 10:56:51 +0200

    add advance and test comments

--- nmcb/src/main/scala/examples/FSM.scala
+++ nmcb/src/main/scala/examples/FSM.scala
@@ -14,28 +14,31 @@ object FSM:
     trait Shipped   extends StateType
     trait Delivered extends StateType
 
-    enum StateValue derives CanEqual:
-      case PLACED
-      case PAID
-      case SHIPPED
-      case DELIVERED
+    enum StateValue[State] derives CanEqual:
+      case PLACED    extends StateValue[Placed]
+      case PAID      extends StateValue[Paid]
+      case SHIPPED   extends StateValue[Shipped]
+      case DELIVERED extends StateValue[Delivered]
 
     import StateValue.*
 
     def make(item: String, price: Double): Order[Placed] =
-      Order[Placed](item = item, price = price, state = PLACED)
+      Order(item = item, price = price, state = PLACED)
 
 
-  case class Order[State <: StateType](item: String, price: Double, state: StateValue):
+  case class Order[State <: StateType] private (item: String, price: Double, state: StateValue[State]):
+
+    private def advance[Next <: StateType](next: StateValue[Next]): Order[Next] =
+      copy(state = next)
 
     def pay(using ev: State =:= Placed): Order[Paid] =
-      copy[Paid](state = PAID)
+      advance(PAID)
 
     def ship(using ev: State =:= Paid): Order[Shipped] =
-      copy[Shipped](state = SHIPPED)
+      advance(SHIPPED)
 
     def deliver(using ev: State =:= Shipped): Order[Delivered] =
-      copy[Delivered](state = DELIVERED)
+      advance(DELIVERED)
 
 
   object Client:
@@ -44,4 +47,3 @@ object FSM:
     private val order2 = order1.pay
     private val order3 = order2.ship
     private val order4 = order3.deliver
-
--- nmcb/src/test/scala/examples/TestFSM.scala
+++ nmcb/src/test/scala/examples/TestFSM.scala
@@ -10,16 +10,16 @@ class TestFSM extends AnyFunSuite:
     assertCompiles(
       """import FSM.*
         |val order1 = Order.make(item = "chair", price = 666.00)
-        |val order2 = order1.pay      // (1)
-        |val order3 = order2.ship     // (2)
-        |val order4 = order3.deliver  // (3)
+        |val order2 = order1.pay      // (1) Compiles
+        |val order3 = order2.ship     // (2) Compiles
+        |val order4 = order3.deliver  // (3) Compiles
         |""".stripMargin)
 
     /** incorrect transition order */
     assertDoesNotCompile(
       """import FSM.*
         |val order1 = Order.make(item = "chair", price = 666.00)
-        |val order2 = order1.pay      // (1)
-        |val order3 = order2.deliver  // (3)
+        |val order2 = order1.pay      // (1) Compiles
+        |val order3 = order2.deliver  // (3) ERROR: Cannot prove that examples.FSM.Order.Paid =:= examples.FSM.Order.Shipped.
         |val order4 = order3.ship     // (2)
         |""".stripMargin)
